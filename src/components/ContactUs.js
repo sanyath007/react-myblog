2cb8439ef1537e6fc7df4ed88f7623758dfab8fb
Sanyath Thammawong <sanyath007@gmail.com>
2020-08-20 23:33:23 +0700

    Created main components

---
+++ src/components/ContactUs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      This is Contact us page.
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      This is Login page.
+    </div>
+  )
+}
+
+export default Home
