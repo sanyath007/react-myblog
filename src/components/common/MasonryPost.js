2ecaed8444d97d20e02855403df1e4b22b47444a
Sanyath Thammawong <sanyath007@gmail.com>
2020-08-22 16:24:39 +0700

    Modified Home with adding featured from mock and styling post for responsively display

--- src/components/common/MasonryPost.js
+++ src/components/common/MasonryPost.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { categoryColors } from './styles';
 
-function MasonryPost({post, tagOnTop}) {
+function MasonryPost({post, tagsOnTop}) {
+  const windowWidth = window.innerWidth;
   const imageBackground = { backgroundImage: `url("${require(`../../assets/images/${post.image}`)}")`};
-  const style = {...imageBackground, ...post.style};
+  const style = windowWidth > 900 ? {...imageBackground, ...post.style} : imageBackground;
   
   return (
     <a className="masonry-post overlay" style={style} href={post.link}>
-      <div className="image-text">
+      <div className="image-text" style={{justifyContent: tagsOnTop ? 'space-between' : 'flex-end'}}>
         <div className="tags-container">
           {post.categories.map((tag, index) =>
             <span key={index} className="tag" style={{backgroundColor: categoryColors[tag]}}>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,23 @@
 import React from 'react';
+import featured from '../assets/mocks/featured';
 import trending from '../assets/mocks/trending';
 import PostMasonry from '../components/common/PostMasonry';
+import MasonryPost from '../components/common/MasonryPost';
+
+const featuredConfig = {
+  0: {
+    gridArea: '1 / 1 / 2 / 3',
+    height: '300px'
+  },
+  1: {
+    height: '300px'
+  },
+  3: {
+    height: '630px',
+    marginLeft: '30px',
+    width: '630px'
+  }
+};
 
 const trendingConfig = {
   1: {
@@ -15,12 +32,21 @@ const mergeStyles = function(posts, config) {
 };
 
 mergeStyles(trending, trendingConfig);
+mergeStyles(featured, featuredConfig);
+
+const lastFeatured = featured.pop();
 
 function Home() {
   return (
     <section className="container home">
       <div className="row">
-        <h2>Trending Posts</h2>
+        <h1>Featured Posts</h1>
+        <section className="featured-posts-container">
+          <PostMasonry posts={featured} columns={2} tagsOnTop={true} />
+          <MasonryPost post={lastFeatured} tagsOnTop={true} />
+        </section>
+
+        <h1>Trending Posts</h1>
         <PostMasonry posts={trending} columns={3} />
       </div>
     </section>
