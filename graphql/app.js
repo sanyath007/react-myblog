4a03a8bc8636f4eb0b1d8c063fdda30d35c3c2e8
Sanyath Thammawong <sanyath007@gmail.com>
2020-09-06 15:59:03 +0700

    Setup backend with creating folder graphql and adding app.js and schemas/index.js

---
+++ graphql/app.js
@@ -0,0 +1,17 @@
+const Koa = require('koa')
+const { ApolloServer } = require('apollo-server-koa')
+const { makeExecutableSchema } = require('graphql-tools')
+const { typeDefs, resolvers } = require('./schemas')
+const PORT = 4000
+
+const server = new ApolloServer({
+  schema: makeExecutableSchema({ typeDefs, resolvers })
+})
+
+const app = new Koa()
+
+server.applyMiddleware({ app })
+
+app.listen({ post: PORT }, () => {
+  console.log(`Server ready at http://localhost:${PORT + server.graphqlPath}`);
+})
